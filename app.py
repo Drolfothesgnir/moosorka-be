7f25a9aa3bb8baa3c183f449881afea3815e3637
Drolfothesgnir <kyryl.yeletsky@gmail.com>
2024-10-17 14:47:16 +0300

    added dev and prod environments

--- app.py
+++ app.py
@@ -22,9 +22,12 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = (
         "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     )
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-        "DATABASE_URL", "sqlite:///data.db"
-    )
+    if os.getenv("FLASK_ENV") == "development":
+        # Use SQLite in development
+        app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    else:
+        # Use PostgreSQL in production (e.g., Docker)
+        app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
